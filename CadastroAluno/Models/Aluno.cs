5264b9f2d37ae4dd2e530e6ca4954ead9430d1b5
GABRIEL VARUZZA MALTA <gabriel.vmalta@senacsp.edu.br>
2022-09-29 21:47:25 -0300

    Criada a Model Aluno.

---
+++ CadastroAluno/Models/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroAluno.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Turma { get; set; }
+        public double Media { get; set; }
+
+        public void AtualizarDados(string nome, string turma)
+        {
+            Nome = nome;
+            Turma = turma;
+        }
+
+        public bool VeriicaAprovacao()
+            => Media > 5;
+
+        public void AtualizaMedia(double novaMedia)
+        {
+            Media = novaMedia;
+        }
+    }
+}
