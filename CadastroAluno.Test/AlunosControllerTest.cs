7fc6b3bd4af8d4cd7cbdf6f0ade6e8e908d18428
GABRIEL VARUZZA MALTA <gabriel.vmalta@senacsp.edu.br>
2022-10-06 20:13:39 -0300

    Etapa 2 - Escrita de Testes concluída.

--- CadastroAluno.Test/AlunosControllerTest.cs
+++ CadastroAluno.Test/AlunosControllerTest.cs
@@ -26,17 +26,129 @@ namespace CadastroAluno.Test
         }
 
         [Fact]
-        public void IndexRetornaOuNaoRetorna()
+        public void IndexRetornaViewResult()
         {
             //act
             var result = _controller.Index();
 
             //assert
-            var okResult = Assert.IsType<ViewResult>(result);
-
-
+            Assert.IsType<ViewResult>(result);
             Assert.NotNull(result);
         }
 
+        [Fact]
+        public void IndexChamaRepositoryUmaVez()
+        {
+            //arrange
+            var controller = new AlunosController(_repository.Object);
+            _repository.Setup(a => a.GetAlunos());
+
+            //act
+            controller.Index();
+
+            //assert
+            _repository.Verify(ar => ar.GetAlunos(), Times.Once);
+        }
+
+        [Fact]
+        public void DetailsRetornaBadRequestParaIdNulo()
+        {
+            //Arrange
+            AlunosController controller = _controller;
+
+
+            //Act
+            var result = controller.Details(0);
+
+            //Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public void DetailsRetornaNotFoundParaIdValido()
+        {
+            //arrange
+            AlunosController controller = _controller;
+
+            //act
+            var result = controller.Details(30);
+
+            //assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void DetailsRetornaViewResultParaAlunoEncontrado()
+        {
+            //arrange
+            AlunosController controller = _controller;
+
+            Aluno aluno = new Aluno();
+            aluno.Id = 1;
+
+            _repository.Setup(a => a.GetAlunoById(1))
+               .Returns(aluno);
+
+            //act
+            var result = controller.Details(1);
+
+            //assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void DetailsChamaRepositoryUmaVez()
+        {
+            //arrange
+            AlunosController controller = _controller;
+
+            Aluno aluno = new Aluno();
+            aluno.Id = 2;
+
+            _repository.Setup(a => a.GetAlunoById(2))
+                .Returns(aluno);
+
+            //act
+            controller.Details(2);
+
+
+            //assert
+            _repository.Verify(ar => ar.GetAlunoById(2), Times.Once);
+        }
+
+        [Fact]
+        public void CreateSempreRetornaView()
+        {
+            //Arrange
+            AlunosController controller = _controller;
+
+            //Act
+            var result = controller.Create();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void CreateVerificaSeRepositorioFoiChamadoUmaVezERetornaRedirectToAction()
+        {
+            //arrange
+            AlunosController controller = _controller;
+
+            Aluno aluno = new Aluno();
+
+            aluno.Id = -5;
+            aluno.Nome = null;
+            aluno.Turma = null;
+            aluno.Media = -15;
+
+            //act
+            var total = controller.Create(aluno);
+
+            //assert
+            _repository.Verify(ar => ar.AddAluno(aluno), Times.Once);
+
+            Assert.IsType<RedirectToActionResult>(total);
+        }
     }
 }
